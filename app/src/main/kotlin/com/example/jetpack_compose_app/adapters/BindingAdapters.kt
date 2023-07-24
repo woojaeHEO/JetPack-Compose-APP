2d61df970dc72c685fd7d16be403eb58d27bf683
Woojae Heo <h69088@gmail.com>
2023-07-24 17:49:57 +0900

    DataBinding - adapter 추가

---
+++ app/src/main/kotlin/com/example/jetpack_compose_app/adapters/BindingAdapters.kt
@@ -0,0 +1,13 @@
+package com.example.jetpack_compose_app.adapters
+
+import android.view.View
+import androidx.databinding.BindingAdapter
+
+@BindingAdapter("isGone")
+fun bindIsGone(view: View, isGone: Boolean) {
+    view.visibility = if (isGone) {
+        View.GONE
+    } else {
+        View.VISIBLE
+    }
+}
---
+++ app/src/main/kotlin/com/example/jetpack_compose_app/adapters/PlantDetailBindingAdapters.kt
@@ -0,0 +1,41 @@
+package com.example.jetpack_compose_app.adapters
+
+import android.text.method.LinkMovementMethod
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.text.HtmlCompat
+import androidx.core.text.HtmlCompat.FROM_HTML_MODE_COMPACT
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.example.jetpack_compose_app.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+@BindingAdapter("imageFromUrl")
+fun bindImageFromUrl(view: ImageView, imageUrl: String?) {
+    if (imageUrl.isNullOrEmpty()) {
+        return
+    }
+    Glide.with(view)
+        .load(imageUrl)
+        .placeholder(android.R.drawable.progress_indeterminate_horizontal)
+        .into(view)
+}
+
+@BindingAdapter("isFabGone")
+fun bindIsFabGone(view: FloatingActionButton, isGone: Boolean?) {
+    if (isGone == null || isGone) {
+        view.hide()
+    } else {
+        view.show()
+    }
+}
+
+@BindingAdapter("renderHtml")
+fun bindRenderHtml(view: TextView, description: String?) {
+    if (description != null) {
+        view.text = HtmlCompat.fromHtml(description, FROM_HTML_MODE_COMPACT)
+        view.movementMethod = LinkMovementMethod.getInstance()
+    } else {
+        view.text = ""
+    }
+}
